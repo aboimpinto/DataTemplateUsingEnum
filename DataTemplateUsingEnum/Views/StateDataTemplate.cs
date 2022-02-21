9880daabacf158dcaf59a3f09b1161b32a0b9638
Paulo Aboim Pinto <paulo.pinto@hemotune.ch>
2022-02-21 14:43:31 +0100

    Adding Avalonia way of choose DataTemplates

---
+++ DataTemplateUsingEnum/Views/StateDataTemplate.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia.Controls;
+using Avalonia.Controls.Templates;
+using Avalonia.Metadata;
+using DataTemplateUsingEnum.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DataTemplateUsingEnum.Views
+{
+    internal class StateDataTemplate : IDataTemplate
+    {
+        [Content]
+        public Dictionary<string, IDataTemplate> Templates { get; } = new Dictionary<string, IDataTemplate>();
+
+        public IControl Build(object param)
+        {
+            var key = param.ToString();
+            if (string.IsNullOrEmpty(key))
+            {
+                throw new ArgumentNullException(nameof(param));
+            }
+
+            return this.Templates[key].Build(param);
+        }
+
+        public bool Match(object data)
+        {
+            var key = data.ToString();
+
+            return data is State &&
+                !string.IsNullOrEmpty(key) &&
+                this.Templates.ContainsKey(key);
+        }
+    }
+}
